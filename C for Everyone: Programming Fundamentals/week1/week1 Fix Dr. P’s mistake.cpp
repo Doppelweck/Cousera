78f34f7951acfe8a1352eafa26af2031625ba29f
Sebastian Friedrich <109851621+Doppelweck@users.noreply.github.com>
2022-10-11 13:28:50 +0200

    Create week1 Fix Dr. P’s mistake.cpp

---
+++ C for Everyone: Programming Fundamentals/week1/week1 Fix Dr. P’s mistake.cpp
@@ -0,0 +1,20 @@
+/*
+Peer-graded Assignment: Fix Dr. P’s mistake (week 1)
+
+The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+
+#include<stdio.h>
+int main(void)
+{   
+    printf(‘ hello world\n’);
+    return 0;
+}  
+
+*/
+
+#include <stdio.h>
+int main(void)
+{   
+    printf(" hello world\n");
+    return 0;
+}    
